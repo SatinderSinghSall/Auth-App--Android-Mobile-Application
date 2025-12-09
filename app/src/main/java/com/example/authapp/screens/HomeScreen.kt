a459107dc820dc68135a7e450d5d09fab4692e53
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-12-09 18:32:11 +0530

    Implemented Home screen with logout functionality.

--- app/src/main/java/com/example/authapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/authapp/screens/HomeScreen.kt
@@ -1,11 +1,51 @@
 package com.example.authapp.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.example.authapp.AuthState
 import com.example.authapp.AuthViewModel
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel) {
-    
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController? = null,
+    authViewModel: AuthViewModel? = null) {
+    val authState = authViewModel?.authState?.observeAsState()
+
+    LaunchedEffect(authState?.value) {
+        when(authState?.value) {
+            is AuthState.Unauthenticated -> navController?.navigate("login")
+            else -> Unit
+        }
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Text(text = ("Home Page"))
+
+        TextButton(onClick = {
+            authViewModel?.signout()
+        }) {
+            Text(text = "Logout")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
 }
